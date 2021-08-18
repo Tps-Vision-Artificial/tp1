464292b7e95e7910ef7fd40701fdd0e79388ecca
ignam <ignacio.mendez@ing.austral.edu.ar>
2021-08-18 15:38:15 -0300

    version 2.0

--- src/detection_main.py
+++ src/detection_main.py
@@ -7,6 +7,7 @@ def main(val):
     window_name = 'Window'
     threshold_trackbar_name = 'Trackbar'
     difference_trackbar_name = 'Difference'
+    radius_trackbar_name = 'Radius'
     slider_max = 151
     cv.namedWindow(window_name)
     cap = cv.VideoCapture(0)
@@ -14,38 +15,40 @@ def main(val):
     color_white = (255, 255, 255)
 
     create_trackbar(threshold_trackbar_name, window_name, slider_max)
-    create_trackbar(difference_trackbar_name, window_name, 1)
+    create_trackbar(difference_trackbar_name, window_name, 100)
+    create_trackbar(radius_trackbar_name,window_name,30)
 
     saved_contours = []
     while True:
         ret, frame = cap.read()
         frame = cv.flip(frame, 1)
 
-        #1
+        # 1
         gray_frame = apply_color_convertion(frame=frame, color=cv.COLOR_BGR2GRAY)
 
-        #2
+        # 2
         trackbar_val = get_trackbar_value(trackbar_name=threshold_trackbar_name, window_name=window_name)
         adapt_frame = adaptive_threshold(frame=gray_frame, slider_max=slider_max,
                                          adaptative=cv.ADAPTIVE_THRESH_GAUSSIAN_C,
                                          binary=cv.THRESH_BINARY,
                                          trackbar_value=trackbar_val)
-        #3
-        frame_denoised = denoise(frame=adapt_frame, method=cv.MORPH_ELLIPSE, radius=10)
+        # 3
+        radius = get_trackbar_value(trackbar_name=radius_trackbar_name,window_name=window_name)
+        frame_denoised = denoise(frame=adapt_frame, method=cv.MORPH_ELLIPSE, radius=radius)
 
-        #4 Contours
+        # 4 Contours
         contours = get_contours(frame=frame_denoised, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)
         if len(contours) > 0:
             biggest_contour = get_biggest_contour(contours=contours)
-            trackbar_val2 = get_trackbar_value(trackbar_name=difference_trackbar_name, window_name=window_name)
-            if compare_contours(contour_to_compare=biggest_contour, saved_contours=saved_contours, max_diff=trackbar_val2):
-                draw_contours(frame=frame_denoised, contours=biggest_contour, color=(0,0,0), thickness=20)
+            max_diff = get_percentage(trackbar_name=difference_trackbar_name, window_name=window_name)
+            if compare_contours(contour_to_compare=biggest_contour, saved_contours=saved_contours, max_diff=max_diff):
+                draw_contours(frame=frame_denoised, contours=biggest_contour, color=(0, 0, 0), thickness=20)
             draw_contours(frame=frame_denoised, contours=biggest_contour, color=color_white, thickness=3)
         cv.imshow('Window', frame_denoised)
 
         if cv.waitKey(1) & 0xFF == ord('k'):
             if biggest_contour is not None:
-                #usar un dict (el HashMap de Python) para poder ponerle un nombre
+                # usar un dict (el HashMap de Python) para poder ponerle un nombre
                 saved_contours.append(biggest_contour)
 
         if cv.waitKey(1) & 0xFF == ord('q'):
@@ -63,6 +66,10 @@ def on_trackbar(val):
 
 def get_trackbar_value(trackbar_name, window_name):
     return int(cv.getTrackbarPos(trackbar_name, window_name) / 2) * 2 + 3
+
+
+def get_percentage(trackbar_name, window_name):
+    return int(cv.getTrackbarPos(trackbar_name, window_name) / 100)
 
 
 def denoise(frame, method, radius):
@@ -85,6 +92,7 @@ def draw_contours(frame, contours, color, thickness):
     cv.drawContours(frame, contours, -1, color, thickness)
     return frame
 
+
 def get_contours(frame, mode, method):
     contours, hierarchy = cv.findContours(frame, mode, method)
     return contours
@@ -103,5 +111,6 @@ def compare_contours(contour_to_compare, saved_contours, max_diff):
         if cv.matchShapes(contour_to_compare, contour, cv.CONTOURS_MATCH_I2, 0) < max_diff:
             return True
     return False
+
 
 main(0)
